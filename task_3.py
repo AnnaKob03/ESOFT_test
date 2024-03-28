cb2c2fbb167b29e6edd2c7c8b57a307a0fe50e1b
AnnaKob03 <165302885+AnnaKob03@users.noreply.github.com>
2024-03-28 21:36:53 +0500

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,19 @@
+def is_valid(s):
+    stack = []
+    mapping = {')': '(', ']': '[', '}': '{'}
+
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping.keys():
+            if not stack or mapping[char] != stack.pop():
+                return False
+
+    return not stack
+
+# примеры использования
+print(is_valid("()"))
+print(is_valid("()[]{}"))
+print(is_valid("(]"))
+print(is_valid("([)]"))
+print(is_valid("{[]}"))
